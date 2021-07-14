ec4bf441e344be04bb5d50f1ff9177c86495bee5
skzz <16473053@qq.com>
2021-07-14 14:04:05 +0800

    v4.0 增加rabbitmq

--- test_maven/src/main/java/redis/RedisTest.java
+++ test_maven/src/main/java/redis/RedisTest.java
@@ -14,7 +14,12 @@ public class RedisTest {
 
     public static void main(String[] args) {
         //连接本地的 Redis 服务
-        Jedis jedis = new Jedis("192.168.200.10");
+        //Jedis jedis = new Jedis("192.168.200.10");
+        Jedis jedis = new Jedis("aliyun",6380);
+
+
+        String name =jedis.get("name");
+        System.out.println("name:"+name);
         System.out.println("连接成功");
         //查看服务是否运行
         System.out.println("服务正在运行: "+jedis.ping());
--- test_maven/src/main/java/test/test2.java
+++ test_maven/src/main/java/test/test2.java
@@ -1,4 +1,38 @@
 package test;
 
+import static com.google.common.collect.ComparisonChain.start;
+
 public class test2 {
+
+    public static void main(String[] args) {
+
+
+        System.out.printf("--"+colorEnum.RED);
+        ThreadLocal myThreadLocal = new ThreadLocal();
+        ThreadLocal myThreadLoca2 = new ThreadLocal();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                myThreadLocal.set("hello1");
+                System.out.printf("---"+myThreadLocal.get());
+                myThreadLoca2.set("1111hello1");
+                System.out.printf("---"+myThreadLoca2.get());
+            }
+        }).start();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                myThreadLocal.set("hello2");
+                System.out.printf("---"+myThreadLocal.get());
+                myThreadLoca2.set("2222hello1");
+                System.out.printf("---"+myThreadLoca2.get());
+            }
+        }).start();
+
+    }
+
+
+
 }
